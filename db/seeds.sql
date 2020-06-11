05beae78091d72379f67d4532c372d402c1af365
Cheryl Daniels <cheryld433@gmail.com>
2020-06-11 09:50:59 -0400

    added seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name) VALUES ("Clasic Hamburger");
+INSERT INTO burgers (burger_name) VALUES ("Classic Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Portobello Mushroom Burger", true);
+
+
+
