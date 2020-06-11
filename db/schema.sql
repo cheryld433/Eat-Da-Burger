94e7f1fe920f726c0b9dc79944758b5340b44862
Cheryl Daniels <cheryld433@gmail.com>
2020-06-11 09:50:14 -0400

    addedd schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXIST burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(250) NOT NULL,
+    devoured BOOLEAN DEFAULT false, 
+    PRIMARY KEY (id)
+);
+
